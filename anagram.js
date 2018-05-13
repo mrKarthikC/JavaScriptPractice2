7866d56e202e8fc94e0efa6d1023dde67350f783
Karthik Chilukuri <37879494+mrKarthikC@users.noreply.github.com>
2018-05-13 18:08:34 +0530

    2 strings anagram or not

--- anagram.js
+++ anagram.js
@@ -1,22 +1,22 @@
 function anagram(myStr, myStr2)
 {
     var flag = true;
-    var myStrArr = myStr.split('');
-    var myStr2Arr = myStr2.split('');
-    myStrArr.sort();
-    myStr2Arr.sort();
-    for (var i = 0; i < myStr2Arr.length; i++) {
-        if (myStrArr[i] != myStr2Arr[i]) {
+    var Arr1 = myStr.split('');
+    var Arr2 = myStr2.split('');
+    Arr1.sort();
+    Arr2.sort();
+    for (var i = 0; i < Arr2.length; i++) {
+        if (Arr1[i] != Arr2[i]) {
             flag = false;
         }
     }
         if (flag) {
-            console.log("The strings : " + myStr + " and " + myStr2 + ", are Anagrams");
+            console.log("The strings : " + myStr + " and " + myStr2 + ", are Anagrams.");
     }       else {
-                 console.log("The strings : " + myStr + " and " + myStr2 + ", aren't Anagrams");
+                 console.log("The strings : " + myStr + " and " + myStr2 + ", aren't Anagrams.");
     }
 }
 
-var myStr = "Bugatti";
-var myStr2 = "Bentley";
-anagram(myStr, myStr2);
+var Arr1 = "Bugatti";
+var Arr2 = "Bentley";
+anagram(Arr1, Arr2);
